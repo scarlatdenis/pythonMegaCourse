418c6101a0a4c49eedd8ea61d172981e1a600821
Denis <denis.scarlat@gmail.com>
2022-03-18 22:04:54 +0200

    co

--- 080_Jupyter.py
+++ 080_Jupyter.py
@@ -1,2 +1,18 @@
 # to start jupyter notebook type:     python -m notebook
 
+import os
+import time
+
+def path(a):
+    while True:
+            if os.path.exists(a):
+                print("File exist")
+                time.sleep(3)
+                print("i am happy")
+                break
+            else:
+                print("I can`t find your file :(")
+                break
+
+path("files/fruits.txt")
+
