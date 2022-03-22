9c9e7850fa21e266e2ef69ca3f08ec5aa54f23e8
denis <denis.scarlat@gmail.com>
2022-03-22 23:21:38 +0200

    commited from mac

--- mapping/map1.py
+++ mapping/map1.py
@@ -1,10 +1,16 @@
 import folium
-map = folium.Map(location=[38.88, - 99.09],
-                 zoom_start=6, tiles="Mapbox Bright")
-map.save("Map1.html")
 
-map.add_child(folium.Marker(location=[
-              48.2, -99.1], popup="Hi, i am a Marker", icon=folium.Icon(color="green")))
+map = folium.Map(location=[47.022496, 28.770363],
+                 zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+
+fg.add_child(folium.Marker(location=[
+    47.022, 28.77], popup="Here is Larisa", icon=folium.Icon(color="green")))
+
+map.add_child(fg)
+
+map.save("Map1.html")
 
 
 # in terminal write: python3 map1.py
