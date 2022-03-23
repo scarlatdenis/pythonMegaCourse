31e55a528a471fec19f1bfabd619c672d43117f0
denis <denis.scarlat@gmail.com>
2022-03-23 23:57:54 +0200

    added some dinamic markers in my map

---
+++ mapping/120_File_procesing.py
@@ -0,0 +1,5 @@
+import pandas
+
+data = pandas.read_csv("118 Volcanoes.txt")
+
+print(data)
--- mapping/map1.py
+++ mapping/map1.py
@@ -1,12 +1,20 @@
+
 import folium
+import pandas
+
+data = pandas.read_csv("118 Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 
 map = folium.Map(location=[47.022496, 28.770363],
                  zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-fg.add_child(folium.Marker(location=[
-    47.022, 28.77], popup="Here is Larisa", icon=folium.Icon(color="green")))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln],
+                 popup="I am a Marker", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
