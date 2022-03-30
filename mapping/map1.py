fc49e6264db3082e62cf25ff25fbaf5d7f7e7882
denis <denis.scarlat@gmail.com>
2022-03-30 22:26:27 +0300

    created dynamic color function for Marker

--- mapping/map1.py
+++ mapping/map1.py
@@ -2,19 +2,29 @@
 import folium
 import pandas
 
-data = pandas.read_csv("118 Volcanoes.txt")
+data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 
-map = folium.Map(location=[47.022496, 28.770363],
-                 zoom_start=6, tiles="Stamen Terrain")
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation >= 3000:
+        return "orange"
+    else:
+        return "red"
+
+
+map = folium.Map(location=[38.1997986, -99.1809998],
+                 zoom_start=4, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(location=[lt, ln],
-                 popup="I am a Marker", icon=folium.Icon(color="green")))
+                 popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
