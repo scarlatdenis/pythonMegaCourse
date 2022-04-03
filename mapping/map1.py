80ff5246115d7b90efa479eecc26c2daf6b26fce
denis <denis.scarlat@gmail.com>
2022-04-03 19:41:47 +0300

    created dynamic color function for Marker

--- mapping/map1.py
+++ mapping/map1.py
@@ -1,4 +1,6 @@
 
+from textwrap import fill
+from turtle import color, fillcolor
 import folium
 import pandas
 
@@ -23,10 +25,14 @@ map = folium.Map(location=[38.1997986, -99.1809998],
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln],
-                 popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
+    fg.add_child(folium.Marker(location=[lt, ln], radius=6, popup=str(el) + " m",
+                               fill_color=color_producer(el), color="grey", fill_opacity=0.7))
 
-map.add_child(fg)
+fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig"),
+                            style_function=lambda x: {'fillColor': 'yellow'}))
+
+
+map.add_child(fg) 
 
 map.save("Map1.html")
 
