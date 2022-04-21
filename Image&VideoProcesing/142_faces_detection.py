0669ba0048d66d4ae7a9877b1896310907383b2c
denis <denis.scarlat@gmail.com>
2022-04-21 23:54:08 +0300

    created

--- Image&VideoProcesing/142_faces_detection.py
+++ Image&VideoProcesing/142_faces_detection.py
@@ -1,15 +1,18 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier(
-    "images/faces/haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier("/Image&VideoProcesing/images/faces/haarcascade_frontalface_default.xml")
 
-img = cv2.imread("images/faces/photo.jpg")
+img = cv2.imread("/Image&VideoProcesing/images/faces/photo.jpg")
+
+cv2.imshow("Gray",img)
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.05 )
+
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05)
 
 cv2.imshow("Gray", gray_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
 
